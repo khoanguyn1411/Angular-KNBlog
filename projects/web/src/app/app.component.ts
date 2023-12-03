37fe148e78c2ba6960ff781ff748ad974496abb4
khoanguyn1411 <khoaah1411@gmail.com>
2023-12-03 15:42:29 +0700

    Complete config eslint for app

---
+++ projects/web/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'web';
+
+  title$ = new Observable()
+}
