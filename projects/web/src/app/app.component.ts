4466afc01af7ea9b606c3fc49c70d0b8d446f656
khoanguyn1411 <khoaah1411@gmail.com>
2024-05-01 17:07:40 +0700

    Update eslint config

--- projects/web/src/app/app.component.ts
+++ projects/web/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Observable } from 'rxjs';
 
 @Component({
-  selector: 'knc-root',
+  selector: 'knw-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
