bf0f647bf95138d2d5b357630d95909ce89c54da
khoanguyn1411 <khoaah1411@gmail.com>
2024-06-02 19:11:40 +0700

    Complete avatar component

--- projects/common/src/lib/shared/components/avatar/avatar.component.ts
+++ projects/common/src/lib/shared/components/avatar/avatar.component.ts
@@ -7,17 +7,28 @@ import { User } from '@knb/core/models/user';
   standalone: true,
   templateUrl: './avatar.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  styleUrls: ['./avatar.component.scss']
+  styleUrls: ['./avatar.component.scss'],
 })
 export class AvatarComponent {
-  public readonly user = input<User | null>(null)
-  public readonly size = input<"small" | "medium" | "large">("medium")
+  public readonly user = input<User | null>(null);
+  public readonly size = input<'small' | 'medium' | 'large'>('medium');
 
   protected get firstLetterOfName() {
-    const user = this.user()
-    if(user == null){
-      return "Anonymous"
+    const user = this.user();
+    if (user == null) {
+      return 'Anonymous';
     }
-    return user.fullName.charAt(0).toUpperCase()
+    return user.fullName.charAt(0).toUpperCase();
+  }
+
+  /** Generate a random color, which is good fit with white text. */
+  protected getRandomColor(): string {
+    const colors: string[] = [
+      '#000080', '#006400', '#800020', '#36454F', '#301934',
+      '#008080', '#228B22', '#FF4500', '#4169E1', '#DC143C',
+      '#708090', '#556B2F', '#800000', '#4682B4', '#D2691E'
+    ];
+    const randomIndex = Math.floor(Math.random() * colors.length);
+    return colors[randomIndex];
   }
 }
