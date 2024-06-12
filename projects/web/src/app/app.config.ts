0b8b3a39ad01866cca40a90e18ced9532f9d0bba
khoanguyn1411 <khoaah1411@gmail.com>
2024-06-12 21:06:05 +0700

    Add withFetch option

--- projects/web/src/app/app.config.ts
+++ projects/web/src/app/app.config.ts
@@ -1,7 +1,7 @@
 import { ApplicationConfig, Provider } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withFetch, withInterceptorsFromDi } from '@angular/common/http';
 import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
@@ -27,7 +27,7 @@ export const appConfig: ApplicationConfig = {
     ...httpInterceptorProviders,
     provideRouter(routes),
     provideClientHydration(),
-    provideHttpClient(withInterceptorsFromDi()),
+    provideHttpClient(withInterceptorsFromDi(), withFetch()),
     provideAnimations(),
     provideWebAppRoutes(),
     provideSocialOauth(),
