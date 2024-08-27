57085aac374ac03a5f3872ac281e67bcb82754a3
khoanguyn1411 <khoaah1411@gmail.com>
2024-08-27 23:29:44 +0700

    Add user view API call

--- projects/web/src/app/features/user-views/user-views.component.ts
+++ projects/web/src/app/features/user-views/user-views.component.ts
@@ -1,5 +1,12 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component, DestroyRef, inject, signal } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { RouterOutlet } from '@angular/router';
+import { Blog } from '@knb/core/models/blog';
+import { Pagination } from '@knb/core/models/pagination';
+import { BlogsApiService } from '@knb/core/services/api-services/blogs-api.service';
+import { toggleExecutionState } from '@knb/core/utils/rxjs/toggle-execution-state';
+import { Observable } from 'rxjs';
 
 /** User views component. */
 @Component({
@@ -7,7 +14,19 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   templateUrl: './user-views.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AsyncPipe],
   styleUrls: ['./user-views.component.scss'],
 })
-export class UserViewsComponent {}
+export class UserViewsComponent {
+  private readonly blogsApiService = inject(BlogsApiService);
+  private readonly destroyRef = inject(DestroyRef);
+
+  protected readonly blogsPage$: Observable<Pagination<Blog>>;
+  protected readonly isLoading = signal(false);
+
+  public constructor() {
+    this.blogsPage$ = this.blogsApiService
+      .getBlogs()
+      .pipe(toggleExecutionState(this.isLoading), takeUntilDestroyed(this.destroyRef));
+  }
+}
