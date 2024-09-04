5b24e397398fdb4fbb20317aaf597b1e20e848a1
khoanguyn1411 <khoaah1411@gmail.com>
2024-09-04 23:03:30 +0700

    Complete styling like button

--- projects/common/src/lib/shared/components/blog-preview/blog-preview.component.ts
+++ projects/common/src/lib/shared/components/blog-preview/blog-preview.component.ts
@@ -1,6 +1,7 @@
 import { DatePipe } from '@angular/common';
-import { ChangeDetectionStrategy, Component, computed, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, effect, input, signal } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
 import { Blog } from '@knb/core/models/blog';
 import { injectWebAppRoutes } from 'projects/web/src/shared/web-route-paths';
@@ -14,7 +15,7 @@ import { UserPreviewComponent } from '../user-preview/user-preview.component';
   templateUrl: './blog-preview.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
   styleUrl: './blog-preview.component.scss',
-  imports: [AvatarComponent, UserPreviewComponent, RouterModule, MatButtonModule, DatePipe],
+  imports: [AvatarComponent, UserPreviewComponent, RouterModule, MatButtonModule, DatePipe, MatIconModule],
 })
 export class BlogPreviewComponent {
   public readonly blog = input.required<Blog>();
@@ -22,4 +23,41 @@ export class BlogPreviewComponent {
   protected readonly routePaths = injectWebAppRoutes();
 
   protected readonly detailUrl = computed(() => this.routePaths.blogs.children.detail.url({ blogId: this.blog().id }));
+
+  protected readonly totalLike = signal(0);
+  protected readonly isBlogLiked = signal(false);
+
+  /**
+   * Handle like button clicked.
+   * @param event Mouse event.
+   */
+  protected onLikeClicked(event: MouseEvent) {
+    event.stopPropagation();
+    this.isBlogLiked.set(true);
+    this.totalLike.update((likeCount) => likeCount + 1);
+  }
+
+  /**
+   * Handle unlike button clicked.
+   * @param event Mouse event.
+   */
+  protected onUnlikeClicked(event: MouseEvent) {
+    event.stopPropagation();
+    this.isBlogLiked.set(false);
+    this.totalLike.update((likeCount) => likeCount - 1);
+  }
+
+  private setIsPostLikedEffect = effect(
+    () => {
+      // this.isBlogLiked.set(true);
+    },
+    { allowSignalWrites: true },
+  );
+
+  private setTotalLikeEffect = effect(
+    () => {
+      this.totalLike.set(this.blog().emoticonCount);
+    },
+    { allowSignalWrites: true },
+  );
 }
