6a02a4e069c9d175339965116ead568e73560d65
khoanguyn1411 <khoaah1411@gmail.com>
2024-09-07 18:32:02 +0700

    Add SEO for user blogs

--- projects/web/src/app/features/blogs/user-blogs/user-blogs.component.ts
+++ projects/web/src/app/features/blogs/user-blogs/user-blogs.component.ts
@@ -1,18 +1,20 @@
 import { AsyncPipe } from '@angular/common';
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { ActivatedRoute } from '@angular/router';
 import { Blog } from '@knb/core/models/blog';
 import { Pagination } from '@knb/core/models/pagination';
 import { User } from '@knb/core/models/user';
+import { UserApiService } from '@knb/core/services/api-services/user-api.service';
 import { AccumulativeBlogsPageService } from '@knb/core/services/ui-services/accumulative-blogs-page.service';
 import { PlatformService } from '@knb/core/services/ui-services/platform.service';
+import { SeoService } from '@knb/core/services/ui-services/seo.service';
 import { filterNull } from '@knb/core/utils/rxjs/filter-null';
 import { accumulativePagination } from '@knb/core/utils/rxjs/paginate';
 import { toggleExecutionState } from '@knb/core/utils/rxjs/toggle-execution-state';
 import { BlogPreviewComponent } from '@knb/shared/components/blog-preview/blog-preview.component';
 import { USER_ID_PARAM } from 'projects/web/src/shared/web-route-paths';
-import { map, Observable, of, shareReplay } from 'rxjs';
+import { map, Observable, of, shareReplay, switchMap, tap } from 'rxjs';
 
 /** User blogs component. */
 @Component({
@@ -23,11 +25,27 @@ import { map, Observable, of, shareReplay } from 'rxjs';
   imports: [AsyncPipe, BlogPreviewComponent, MatButtonModule],
   styleUrl: './user-blogs.component.scss',
 })
-export class UserBlogsComponent extends AccumulativeBlogsPageService {
+export class UserBlogsComponent extends AccumulativeBlogsPageService implements OnInit {
+  private readonly seoService = inject(SeoService);
+  private readonly userApiService = inject(UserApiService);
+  private readonly activatedRoute = inject(ActivatedRoute);
+
   protected readonly isBrowserOnly = inject(PlatformService).isBrowserOnly;
 
-  private createUserIdStream(): Observable<User['id'] | null> {
-    const route = inject(ActivatedRoute);
+  public ngOnInit(): void {
+    this.createUserIdStream(this.activatedRoute)
+      .pipe(
+        filterNull(),
+        switchMap((userId) => this.userApiService.getUserById(userId)),
+        tap((user) => {
+          this.seoService.addTitle(user.fullName);
+          this.seoService.addTags({ description: user.fullName, imageUrl: user.pictureUrl ?? '' });
+        }),
+      )
+      .subscribe();
+  }
+
+  private createUserIdStream(route: ActivatedRoute): Observable<User['id'] | null> {
     return route.paramMap.pipe(
       map((params) => params.get(USER_ID_PARAM)),
       shareReplay({ refCount: true, bufferSize: 1 }),
@@ -35,7 +53,8 @@ export class UserBlogsComponent extends AccumulativeBlogsPageService {
   }
 
   protected override initializeBlogsPage(): Observable<Pagination<Blog>> {
-    const userId$ = this.createUserIdStream();
+    const route = inject(ActivatedRoute);
+    const userId$ = this.createUserIdStream(route);
     return accumulativePagination(
       // Using `debounceTime(0)` here to make the filter emits only 1 time after all changes has synchronously emitted.
       // eslint-disable-next-line rxjs/finnish
